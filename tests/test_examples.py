3eb1863c7da66cb45caf69f3a9ffaf251b2e3bd5
tds333 <tds333@mailbox.org>
2026-07-13 21:39:56 +0200

    update workflows and depls

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -4,7 +4,7 @@ from pytest_examples import CodeExample, EvalExample, find_examples
 
 @pytest.mark.parametrize(
     "example",
-    find_examples("README.md", "docs"),
+    list(find_examples("README.md", "docs")),
     ids=str,
 )
 def test_examples(example: CodeExample, eval_example: EvalExample) -> None:
